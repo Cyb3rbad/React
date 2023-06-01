7f69fe8fabf80083c2b97e2b55ab30705e83343d
Cyb3rbad <alexeijacgues@gmail.com>
2023-06-01 17:29:08 +0400

    todolist

---
+++ hw-second/src/App.js
@@ -0,0 +1,54 @@
+﻿import { useState } from "react";
+
+const App = () => {
+    const [todoValue, setTodoValue] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    const handleChange = (e) => {
+        setTodoValue(e.target.value);
+    };
+
+    const clearInput = () => {
+        document.getElementById("todoValueInput").value = "";
+        setTodoValue("");
+    };
+
+    const addTodo = () => {
+        setTodos([
+            ...todos,
+            {
+                id: todos.length,
+                value: todoValue,
+            }
+        ]);
+        clearInput();
+    };
+
+    return (
+        <>
+            <div>
+                <h2>Todo List</h2>
+            </div>
+            <input
+                id="todoValueInput"
+                onChange={(e) => handleChange(e)}
+            ></input>
+            <button onClick={() => addTodo()}>Add</button>
+            <p>
+                {todos.length} tasks
+            </p>
+            <ul>
+                {todos.map((todo) => {
+                    return (
+                        <li
+                            key={todo.id}>
+                            {todo.value}
+                        </li>
+                    );
+                })}
+            </ul>
+        </>
+    );
+};
+
+export default App;
